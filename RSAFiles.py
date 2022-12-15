b5b33c7c504cec0dd7bdd639ee8e30be53cc3c39
nguyenyn <yen.nhi.nguyen@ulb.be>
2022-12-15 22:44:00 +0100

    File & alternative message encryption with elliptic curve cryptography

--- RSAFiles.py
+++ RSAFiles.py
@@ -1,30 +1,56 @@
-import rsa
+from Cryptem import Crypt, Encrypt, Encryptor, EncryptFile
 
-# https://stuvel.eu/python-rsa-doc/usage.html#working-with-big-files
+# pip install Cryptem
+# https://pypi.org/project/Cryptem/
 
-# Bob generates a key pair, and gives the public key to Alice. \
-# This is done such that Alice knows for sure that the key is really Bob’s
-(bob_pub, bob_priv) = rsa.newkeys(512)
+"""
+# TODO Single - Session Asymmetric Encryption(public - key and private - key):
 
-# Alice writes a message, and encodes it in UTF-8.\
-# The RSA module only operates on bytes, and not on strings, so this step is necessary.
-#message = 'hello Bob!'.encode('utf8')
+#Communication Receiver:
+crypt = Crypt()  # create Crypt object with new random public and private keys
+public_key = crypt.public_key  # read public key
 
-#@Alice encrypts the message using Bob’s public key, and sends the encrypted message.
-with open('ELECH417_TorProject_2223.pdf', 'rb') as msgfile:
-    crypto = rsa.encrypt(msgfile, bob_pub)
-print('message encrypté')
-print(crypto)
+# TODO Give public_key(the public key) to Sender.
 
-#Bob receives the message, and decrypts it with his private key.
-message = rsa.decrypt(crypto, bob_priv)
-print('message décrypté')
-print(message.decode('utf8'))
+#Communication Sender / Encryptor:
+# encryptor = Encryptor(public_key)  # crete Encryptor object with Receiver's public key
+# cipher = encryptor.Encrypt("Hello there!".encode('utf-8'))  # encrypt a message
 
-#Since Bob kept his private key private, Alice can be sure that he is the only one who can read the message.\
-# Bob does not know for sure that it was Alice that sent the message, since she didn’t sign it.
 
-#RSA can only encrypt messages that are smaller than the key.\
-# A couple of bytes are lost on random padding, and the rest is available for the message itself.\
-# For example, a 512-bit key can encode a 53-byte message (512 bit = 64 bytes,\
-# 11 bytes are used for random padding and other stuff).
+# TODO Transmit cipher to Receiver.
+
+# Communication Receiver:
+plaintext = crypt.Decrypt(cipher).decode('utf-8')  # decrypt message
+print(plaintext)
+"""
+
+
+# TODO File Encryption:
+
+# RSA encryption rather inefficient with big quantities of data,
+# the Crypt and Encryptor classes have functions that implement
+# symmetric AES encryption.The secret AES key is encrypted
+# with asymmetric elliptic curve cryptography and embedded into the file,
+# so that the user can use the file encryption functionality in exactly the same way.
+
+# Sender / Encryptor:
+
+crypt = Crypt()  # create Crypt object with new random public and private keys
+public_key = crypt.public_key  # read public key
+
+# TODO Give public_key(the public key) to Sender.
+
+#Communication Sender / Encryptor:
+path_to_file = ".\\ELECH417_TorProject_2223.pdf"
+path_to_save_encrypted_file = ".\\ELECH417_TorProject_2223_2.pdf"
+encryptor = Encryptor(public_key)  # create Encryptor object with Receiver's public key
+cipher = encryptor.EncryptFile(path_to_file, path_to_save_encrypted_file)  # encrypt file
+
+
+# TODO Transmit the encrypted file to Receiver.
+
+# Communication Receiver:
+path_to_encrypted_file = ".\\ELECH417_TorProject_2223_2.pdf"
+path_to_decrypted_file = ".\\ELECH417_TorProject_2223_3.pdf"
+plaintext = crypt.DecryptFile(path_to_encrypted_file, path_to_decrypted_file)  # decrypt file
+
