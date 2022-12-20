5786ec5d464ce3759bdb7839ea58cd7248f90a76
Maxime Pellichero <96079270+CaptainKobra@users.noreply.github.com>
2022-12-20 12:33:32 +0100

    voici

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+from Contact import Contact
+from Contact_list import Contact_list
+from ClientTCP import ClientTCP
+import sys
+import csv
+
+ip = sys.argv[1]
+port = int(sys.argv[2])
+
+client = ClientTCP(ip, port)
+client.start()
+
+
+# Ouverture du fichier en mode lecture
+with open('Contacts.csv', 'r') as fichier_csv:
+    # Initialisation du gestionnaire de fichier CSV
+    gestionnaire_csv = csv.reader(fichier_csv, delimiter=',')
+    # Lecture des lignes du fichier CSV
+    for ligne in gestionnaire_csv:
+        # Conversion de chaque élément de la ligne en entier (sauf le prénom)
+        ligne = [int(x) if x.isdigit() else x for x in ligne]
+        # Stockage de la ligne dans un tuple
+        tuple_ligne = tuple(ligne)
+        client.new_contact(tuple_ligne)
