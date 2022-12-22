dc2bed230806c220d633581e2b7d5cae4d4456c4
nguyenyn <yen.nhi.nguyen@ulb.be>
2022-12-22 17:58:46 +0100

    RSA update

--- RSA.py
+++ RSA.py
@@ -3,67 +3,60 @@ import re
 # pip install Cryptem
 # https://pypi.org/project/Cryptem/
 
-listIP = ['127.0.0.1', '127.0.0.2', '127.0.0.3']
-
+list_of_nodes = [('127.0.0.1', 5003), ('127.0.0.1', 5004), ('127.0.0.1', 5005)]
 
 # TODO Single - Session Asymmetric Encryption(public - key and private - key):
 
 #Communication Receiver:
-crypt = Crypt()  # create Crypt object with new random public and private keys
-public_key = crypt.public_key  # read public key
-
-crypt2 = Crypt()  # create Crypt object with new random public and private keys
-public_key2 = crypt2.public_key  # read public key
-
 crypt3 = Crypt()  # create Crypt object with new random public and private keys
 public_key3 = crypt3.public_key  # read public key
 
-listKeys = [public_key, public_key2, public_key3]
-listCrypts = [crypt3, crypt2, crypt]
+crypt4 = Crypt()  # create Crypt object with new random public and private keys
+public_key4 = crypt4.public_key  # read public key
+
+crypt5 = Crypt()  # create Crypt object with new random public and private keys
+public_key5 = crypt5.public_key  # read public key
+
+listKeys = [public_key3, public_key4, public_key5]
+listCrypts = [crypt5, crypt4, crypt3]
 
 # TODO Give public_key(the public key) to Sender.
 
 
 #Communication Sender / Encryptor:
 
-def encryptTheMessage(message, i):
-    if i == 0:
+def encrypt_the_message(message, i):
+    if isinstance(message, str):
         message = message.encode('utf-8')
     encryptor = Encryptor(listKeys[i])  # crete Encryptor object with Receiver's public key
-    cipher = encryptor.Encrypt(listIP[i].encode('utf8') +"###".encode('utf8')+ message)  # encrypt a message
+    cipher = encryptor.Encrypt(list_of_nodes[i][0].encode('utf8') + "//".encode('utf8') +
+                               str(list_of_nodes[i][1]).encode('utf8') + " ".encode('utf8') + message)  # encrypt a message
     return cipher
 
 
 message = "Hello there!"
 for i in range(len(listKeys)):
-    message = encryptTheMessage(message, i)
+    message = encrypt_the_message(message, i)
 
 
 # TODO Transmit cipher to Receiver.
 
 # Communication Receiver:
 
-def popIP(plaintext):
-    print('plaintext :', plaintext)
-    ipMatch = re.search(b'\d{0,9}\.\d{0,9}\.\d{0,9}\.\d{0,9}', plaintext) #search for an ip address
-    ip = ipMatch.group(0).decode('utf8') #extract the ip & in string$
-    print('plaintext2 :', plaintext)
-    print('ip :', ip)
-    ipMatch = re.split(b'\d{0,9}\.\d{0,9}\.\d{0,9}\.\d{0,9}###', plaintext) #separate the ip address from the payload
-    ipMatch = ipMatch[1] #keep the payload
-    return (ip, ipMatch)
+def pop_header(plaintext):
+    headerInPlaintext = re.search(b'\d{0,9}\.\d{0,9}\.\d{0,9}\.\d{0,9}//\d{0,9}', plaintext) #search for a header
+    header = headerInPlaintext.group(0) #extract the header
+    splitHeaderPlaintext = re.split(b'\d{0,9}\.\d{0,9}\.\d{0,9}\.\d{0,9}//\d{0,9} ', plaintext) #separate the header from the payload
+    restPlaintext = splitHeaderPlaintext[1] #keep the payload
+    return (header, restPlaintext)
 
-def decryptTheCipher(crypt, cipher):
+def decrypt_the_cipher(crypt, cipher):
     plaintext = crypt.Decrypt(cipher)  # decrypt message
-    (ip, ipMatch) = popIP(plaintext)
-    return (ip, ipMatch)
+    (ip, restPlaintext) = pop_header(plaintext)
+    return (ip, restPlaintext)
 
 for i in range(len(listKeys)):
-    (ip, message) = decryptTheCipher(listCrypts[i], message)
-    print(ip)
+    (ip, message) = decrypt_the_cipher(listCrypts[i], message)
 
 messageDecrypted = message.decode('utf8')
 print(messageDecrypted)
-
-
-
