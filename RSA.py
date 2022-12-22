d7e430ba9cf603e6c991810b65661125f00059a7
nguyenyn <yen.nhi.nguyen@ulb.be>
2022-12-22 11:45:30 +0100

    Merge with Main

--- RSA.py
+++ RSA.py
@@ -3,7 +3,7 @@ import re
 # pip install Cryptem
 # https://pypi.org/project/Cryptem/
 
-list_of_nodes = [("127.0.0.1", 5003), ("127.0.0.1", 5004), ("127.0.0.1", 5005)]
+list_of_nodes = [('127.0.0.1', 5003), ('127.0.0.1', 5004), ('127.0.0.1', 5005)]
 
 # TODO Single - Session Asymmetric Encryption(public - key and private - key):
 
@@ -29,8 +29,8 @@ def encrypt_the_message(message, i):
     if i == 0:
         message = message.encode('utf-8')
     encryptor = Encryptor(listKeys[i])  # crete Encryptor object with Receiver's public key
-    print(list_of_nodes[i][0].encode('utf8') + "//".encode('utf8') + str(list_of_nodes[i][1]).encode('utf8'))
-    cipher = encryptor.Encrypt(list_of_nodes[i][0].encode('utf8') + "//".encode('utf8') + str(list_of_nodes[i][1]).encode('utf8') + "###".encode('utf8') + message)  # encrypt a message
+    cipher = encryptor.Encrypt(list_of_nodes[i][0].encode('utf8') + "//".encode('utf8') +
+                               str(list_of_nodes[i][1]).encode('utf8') + "###".encode('utf8') + message)  # encrypt a message
     return cipher
 
 
@@ -46,9 +46,9 @@ for i in range(len(listKeys)):
 # Communication Receiver:
 
 def pop_IP(plaintext):
-    ipMatch = re.search(b'\d{0,9}\.\d{0,9}\.d\{0,9}\.\d{0,9}', plaintext) #search for an ip address
+    ipMatch = re.search(b'\d{0,9}\.\d{0,9}\.\d{0,9}\.\d{0,9}//\d{0,9}', plaintext) #search for an ip address
     ip = ipMatch.group(0).decode('utf8') #extract the ip & in string
-    ipMatch = re.split(b'\d{0,9}\.\d{0,9}\.\d{0,9}\.\d{0,9}', plaintext) #separate the ip address from the payload
+    ipMatch = re.split(b'\d{0,9}\.\d{0,9}\.\d{0,9}\.\d{0,9}//\d{0,9}###', plaintext) #separate the ip address from the payload
     ipMatch = ipMatch[1] #keep the payload
     return (ip, ipMatch)
 
@@ -59,7 +59,6 @@ def decrypt_the_cipher(crypt, cipher):
 
 for i in range(len(listKeys)):
     (ip, message) = decrypt_the_cipher(listCrypts[i], message)
-    print(message)
     print(ip)
 
 messageDecrypted = message.decode('utf8')
